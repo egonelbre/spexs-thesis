b112e2d4f9fad6d392d582c54435dd1d652995e0
Egon Elbre <egonelbre@gmail.com>
2012-11-12 14:03:21 +0200

    dev

--- make.sh
+++ make.sh
@@ -4,4 +4,3 @@ rm -rf build/*
 cp -r *.tex *.bib chapters figures appendices build
 cd build
 pdflatex --shell-escape main && bibtex main && pdflatex main && pdflatex main
-cp main.pdf ../Thesis.pdf
