0f55ea4f6373e7abe68704ba2e204c86efe66756
Egon Elbre <egonelbre@gmail.com>
2012-11-13 23:36:58 +0200

    first draft

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #latex main && bibtex main && latex main && latex main
 rm -rf build/*
-cp -r *.tex *.bib chapters figures appendices build
+cp -r *.tex *.bib chapters figures diagrams appendices build
 cd build
 pdflatex --shell-escape main && bibtex main && pdflatex main && pdflatex main
+#pdflatex main
+cp main.pdf ../Thesis.pdf
